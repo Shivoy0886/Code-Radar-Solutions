0c538b9a93e76abcafc8907af4761f943ae364af
Shivoy0886 <shivoy0779.becse24@chitkara.edu.in>
2025-02-10 16:31:43 +0530

    Solution to Grade Based on Marks

---
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int m;
+    scanf("%d",&m);
+    if(m>90) printf("A");
+    else if(m>80) printf("B");
+    else if(m>70) printf("C");
+    else if(m>60) printf("D");
+    else if(m<60) printf("F");
+    
+    return 0;
+}
