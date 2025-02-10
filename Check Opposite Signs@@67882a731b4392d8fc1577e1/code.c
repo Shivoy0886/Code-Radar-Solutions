a263c31480f0496b9b27442ae7abfc37d5af59e9
Shivoy0886 <shivoy0779.becse24@chitkara.edu.in>
2025-02-10 11:00:53 +0530

    Solution to Check Opposite Signs

---
+++ Check Opposite Signs@@67882a731b4392d8fc1577e1/code.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    #include <stdio.h>
+
+int main() {
+    int n1,n2;
+    scanf("%d %d",&n1,&n2);
+    if((n1>0 && n2<0) || (n1<0 || n2>0)){
+        printf("True");
+    }
+    else {
+        printf("False");
+    }
+    return 0;
+}
+    return 0;
+}
