413f811f58fc3fe0813f35620ce8a9e4a2b09161
Shivoy0886 <shivoy0779.becse24@chitkara.edu.in>
2025-02-10 17:22:36 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -9,8 +9,10 @@ int main() {
      char c;
     scanf("%d %d %c",&n1,&n2,&c);
     if (c=='+') printf("%d",n1+n2);
-    if (c=='-') printf("%d",n1-n2);
-    if (c=='*') printf("%d",n1*n2);
-    if (c=='/') printf("%.f",(float)n1/n2);
+    else if (c=='-') printf("%d",n1-n2);
+    else if (c=='*') printf("%d",n1*n2);
+    else if (c=='/') printf("%.f",(float)n1/n2);
+    else print("Error");
+
     return 0;
 }
